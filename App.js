dccfd86ee8ecbb6d0139544387bcdb4bd2b03e8f
Lucas Rodrigo Krueger <lucasrkthd@gmail.com>
2022-01-28 13:20:45 +0000

    Flag, Field and Mine

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
+import { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import params from './src/params';
+import Field from './src/components/Field';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  render (){
+    return (
+      <View style={styles.container}>
+        <Text>Starting Mines...</Text>
+        <Text style={styles.instructions}> Grid size:
+          {params.getRowsAmount()}x{params.getColumnsAmount()}
+        </Text>
+        <Field />
+        <Field opened />
+        <Field opened nearMines={1} />
+        <Field opened nearMines={2} />
+        <Field opened nearMines={3} />
+        <Field opened nearMines={6} />
+        <Field mined />
+        <Field mined opened />
+        <Field mined opened exploded />
+        <Field flagged/>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F5FCFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center'
+  },
+  instructions: {
+    width: 20
+  }
 });
